b86d30a39873179ab7cf873936d96cfa636cbbcd
cmstuning <cmstuning@gmail.com>
2017-04-02 13:22:31 +0300

    chances visualization and penalty

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,6 +15,43 @@ export class AppComponent {
 
   }
 
+  getVisualPercent(a, b, correlation) {
+    let p = (Math.round(a / b * 100));
+    let r = 100 - p;
+    // we visually should correlate the chance. Every value above 0 is a penalty. Penalty can be 2 or 3
+    let penalty = 1;
+    if (correlation > 0) {
+       penalty = a > (b / 2) ? 2 : 3;
+    }
+    else {penalty = 0.7}
+    
+    let res = Math.round(r / penalty);
+// console.log('percent:', p, 'inverted', r, 'with penalty', res);
+    return res < 100 ? res:100;
+  }
+
+  getChanceText(relative, total) {
+    let text: string = 'Very Low';
+    
+    if (relative <= 0) {
+      // group = 0;
+      text = 'High'
+    }  
+  
+
+    if (relative > 0 && relative < 3) {
+      // group = 1;
+      text = 'Medium'
+    }
+
+    if (relative >= 4 && relative < total / 3) {
+      // group = 2;
+      text = 'Low'
+    }
+
+      
+    return text;
+}
   toggleLoading() {
     this.loading = !this.loading;
 }  
